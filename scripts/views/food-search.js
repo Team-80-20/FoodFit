11374742e9c0658ad26c8b525a86af04239f29eb
rjtj2007 <teacherman1@gmail.com>
2018-01-18 20:37:23 -0800

    built out food-search.js page

--- scripts/views/food-search.js
+++ scripts/views/food-search.js
@@ -6,7 +6,23 @@ var app = app || {};
 
     const $view = $('#foodSearch-view')
 
-    foodSearchView.init = () => $view.show()
+    foodSearchView.init = () => {$view.show()
+        $('foodSearch-submit').one('click', (e) => {
+            e.preventDefault()
+            submit()
+        })
+    }
+
+    function submit(){
+        let meal = {
+            food: $('#food').val(),
+            minCal: $('#minCal').val(),
+            maxCal: $('#maxCal').val(),
+            healthLable: $('healthLable').val(),
+        }
+        app.Recipe.fetch(meal)
+        .then(() => page('/food-results'))
+    }
 
     module.foodSearchView = foodSearchView
 
