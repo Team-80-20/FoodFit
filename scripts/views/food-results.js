3306199f1f552296483a2bde0f5d8380aa5ca525
devintyler26 <devincunningham@allegropediatrics.com>
2018-01-20 11:25:19 -0800

    creating handlebars

--- scripts/views/food-results.js
+++ scripts/views/food-results.js
@@ -6,7 +6,23 @@ var app = app || {};
 
     const $view = $('#foodResults-view')
 
-    foodResultsView.init = () => $view.show()
+    function RecipeObj(item) {
+        this.label = item.hits.recipe.label
+        this.yield = item.hits.recipe.yield
+        this.image = item.hits.recipe.image
+        this.source = item.hits.recipe.source
+        this.url = item.hits.recipe.url
+        this.ingredientLines = item.hits.recipe.ingredientLines
+        this.calories = item.hits.recipe.calories
+        this.totalNutrients = item.hits.recipe.totalNutrients
+        this.FAT = item.hits.recipe.totalNutrients.FAT.quantity
+        this.CHOCDF = item.hits.recipe.totalNutrients.CHOCDF.quantity
+        this.PROCNT = item.hits.recipe.totalNutrients.PROCNT.quantity        
+    }
+
+    foodResultsView.init = () =>{
+        $view.show()
+    }
 
     module.foodResultsView = foodResultsView
 
