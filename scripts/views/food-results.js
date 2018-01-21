16e1ddc7acacb2f896b4bfe4bd6979de7b1868a2
stariel <ariel.altaras@gmail.com>
2018-01-20 16:05:00 -0800

    recipes can be saved to local storage recipe box

--- scripts/views/food-results.js
+++ scripts/views/food-results.js
@@ -12,8 +12,23 @@ var app = app || {};
         let newResult = template(item)
         $('#foodResults-view').append(newResult)
     })
+    $('.recipe-div').on('click', '.save-recipe', (e) => {
+        const id = $(e.target).data('id')
+        console.log('clicked!', id)
+        saveRecipe(app.recipeResults[id])
+    })
     $view.show()
 }
+
+
+function saveRecipe(recipeResult) {
+    let storedRecipes = []
+    if (localStorage.getItem('recipeBox')) {
+        storedRecipes = JSON.parse(localStorage.getItem('recipeBox'))
+    }
+    storedRecipes.push(JSON.stringify(recipeResult))
+    localStorage.setItem('recipeBox', JSON.stringify(storedRecipes)
+)}
 
     module.foodResultsView = foodResultsView
 
