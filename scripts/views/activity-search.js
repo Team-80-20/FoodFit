c47de2b7e93790618c063425a3233e02dfa759f8
rjtj2007 <teacherman1@gmail.com>
2018-01-20 17:29:12 -0800

    activity-search.js is functioning correctly, needs refinement

--- scripts/views/activity-search.js
+++ scripts/views/activity-search.js
@@ -1,13 +1,24 @@
 var app = app || {};
 
-(module => { 
+(module => {
 
     const activitySearchView= {}
 
     const $view = $('#activitySearch-view')
 
-    activitySearchView.init = () => $view.show()
+    activitySearchView.init = () => {
+        $('img').hide()
+        
+        $('#activitySearch-submit').one('click', (e) => {
+            e.preventDefault()
+
+            let selection = $('#activity-search').val()
+            $(`.${selection}`).show()
+        })
+        $view.show()
+    }
 
     module.activitySearchView = activitySearchView
 
 })(app)
+
