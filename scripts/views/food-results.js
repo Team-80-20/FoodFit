cbc5b18db14bbef5a14458a08724415f7f07f447
stariel <ariel.altaras@gmail.com>
2018-01-22 19:40:08 -0800

    added text to display if search produces no results

--- scripts/views/food-results.js
+++ scripts/views/food-results.js
@@ -11,12 +11,17 @@ var app = app || {};
 
     foodResultsView.init = () =>{
         let resultsRecipe = app.recipeResults
+        if (resultsRecipe.length > 0) {
+        $('#no-recipes').hide()
         $('#foodResults-view').empty()
         let template = Handlebars.compile($('#recipe-template').text())      
         resultsRecipe.forEach(item => {
             let newResult = template(item)
             $('#foodResults-view').append(newResult)
-        })
+        })}
+        else {
+        $('#no-recipes').show()
+        }
         console.log('results page results:', app.recipeResults)
     $('.recipe-div').on('click', '.save-recipe', (e) => {
         const id = $(e.target).data('id')
