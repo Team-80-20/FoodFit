b5135deb09406d15886d721b979a217f8e5df169
stariel <ariel.altaras@gmail.com>
2018-01-25 19:53:39 -0800

    built tdee form and calculator

--- scripts/views/tdee.js
+++ scripts/views/tdee.js
@@ -8,10 +8,29 @@ var app = app || {};
 
     tdeeView.init = () => $view.show()
 
+    $('#tdee-submit').one('click', (e) => {
+        e.preventDefault()
+        tdeeCalc()
+    })
+
+    tdeeCalc = function () {
+        let sex = $('#sex').val()
+        let age = $('#age').val()
+        let height = $('#height').val()
+        let weight = $('#weight').val()
+        let bmr =
+        console.log(sex, age, height, weight)
+        if (sex === 'male') {
+            bmr = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age)
+        }
+        else {
+            bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age)
+        }
+        console.log(bmr)
+        let tdee = bmr * parseFloat($('#activityLevel').val())
+        console.log(tdee)
+        }
+
     module.tdeeView = tdeeView
-
-    tdeeCalc = (age, height, weight, activity) => {
-
-    }
-
+    
 })(app)
